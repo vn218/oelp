83e8f31bb5689c7c720c64ad306eb05acc94d94b
vn218 <72967949+vn218@users.noreply.github.com>
2022-02-25 21:24:15 +0530

    Update sqrt.v

--- design/sqrt.v
+++ design/sqrt.v
@@ -1,21 +1,146 @@
-//Square root by Babylonian Method, not tested tho, just found out about this recently
-//A combinational way to calculate square root
-module sqrt #(parameter WIDTH = 16) (
-  input [WIDTH-1:0] in, 
-  output reg [WIDTH-1:0] out
-);
-  //https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
-  wire [WIDTH-1:0] temp1, temp3, temp5;
-  reg [WIDTH-1:0] temp2, temp4;
-  assign temp1 = in/out;
-  assign temp3 = in/temp2;
-  assign temp5 = in/temp4;
+module sqrt
+  #( parameter I_WIDTH = 16,
+               F_WIDTH = 16)
+  (input [I_WIDTH+F_WIDTH-1:0] N_in,
+   output reg [I_WIDTH+F_WIDTH-1:0] out,
+   input clk, rst, in_valid,
+   output reg out_valid, ready
+  );
+  
+  parameter IDLE = 0,
+            ITERATE = 1,
+            DONE = 2;
+  
+  reg [1:0] state;
+  reg [I_WIDTH+F_WIDTH-1:0] X;
+  wire [I_WIDTH+2*F_WIDTH-1:0] X_in;
+  wire [I_WIDTH+2*F_WIDTH-1:0] X2;
+  
+  reg [I_WIDTH+F_WIDTH-1:0] N;
+  
+  wire [I_WIDTH+2*F_WIDTH-1:0] NX2; 
+  wire [I_WIDTH+F_WIDTH-1:0] R;
+  reg [I_WIDTH+F_WIDTH-1:0] R_old;
+  
+  reg [I_WIDTH+F_WIDTH-1:0] approx;
+  wire [I_WIDTH+2*F_WIDTH-1:0] out_in;
+  
+  // initial approximation of 1/sqrt(N)
+  always @ (*) begin
+    if (N_in > 32'h4000_0000)
+      approx = 32'h0000_0100;
+    
+    if (N_in <= 32'h4000_0000 && N_in > 32'h1000_0000)
+      approx = 32'h0000_0200;
+    
+    if (N_in <= 32'h1000_0000 && N_in > 32'h0400_0000)
+      approx = 32'h0000_0400;
+    
+    if (N_in <= 32'h0400_0000 && N_in > 32'h0100_0000)
+      approx = 32'h0000_0800;
+    
+    if (N_in <= 32'h0100_0000 && N_in > 32'h0040_0000)
+      approx = 32'h0000_1000;
+    
+    if (N_in <= 32'h0040_0000 && N_in > 32'h0010_0000)
+      approx = 32'h0000_2000;
+    
+    if (N_in <= 32'h0010_0000 && N_in > 32'h0004_0000)
+      approx = 32'h0000_4000;
+    
+    if (N_in <= 32'h0004_0000 && N_in > 32'h0001_0000)
+      approx = 32'h0000_8000;
+    
+    if (N_in <= 32'h0001_0000 && N_in > 32'h0000_4000)
+      approx = 32'h0001_0000;
+    
+    if (N_in <= 32'h0000_4000 && N_in > 32'h000_1000)
+      approx = 32'h0002_0000;
+    
+    if (N_in <= 32'h0000_1000 && N_in > 32'h0000_0400)
+      approx = 32'h0004_0000;
+    
+    if (N_in <= 32'h0000_0400 && N_in > 32'h0000_0100)
+      approx = 32'h0008_0000;
+    
+    if (N_in <= 32'h0000_0100 && N_in > 32'h0000_0040)
+      approx = 32'h0010_0000;
+    
+    if (N_in <= 32'h0000_0040 && N_in > 32'h0000_0010)
+      approx = 32'h0020_0000;
+    
+    if (N_in <= 32'h0000_0010 && N_in > 32'h0000_0004)
+      approx = 32'h0040_0000;
+    
+    if (N_in <= 32'h0000_0004)
+      approx = 32'h0080_0000;
+    
+  
+  end
+  
 
-  always @ (in) begin
-    out = {{8{1'b0}},{8{1'b1}}};
+  assign X2 = X*X;
+  assign NX2 = N*X2[I_WIDTH+2*F_WIDTH-1 -: I_WIDTH+F_WIDTH];
+  assign R = 32'h0003_0000 - NX2[I_WIDTH+2*F_WIDTH-1 -: I_WIDTH+F_WIDTH];
+  assign X_in = (X>>1)*R;
+  
+  
+  assign out_in = N*X;
 
-    temp2 = (out + temp1) >> 1;
-    temp4 = (temp2 + temp3) >> 1;
-    out = (temp4 + temp5) >> 1;
-  end 
+  
+  always @ (posedge clk) begin
+    case (state)
+    IDLE : begin
+	  N <= N_in;
+      X <= approx;
+      R_old <= 32'h0003_0000;
+    end
+    ITERATE : begin
+      if (R <= R_old) begin
+        X <= X_in[I_WIDTH+2*F_WIDTH-1 -: I_WIDTH+F_WIDTH];
+        R_old <= R;
+      end
+    end
+    endcase
+  
+  end
+  
+  always @ (posedge clk) begin
+    
+    if (rst) begin
+      state <= IDLE;
+    end
+    else begin
+      case (state)
+      IDLE : begin
+        if (in_valid) begin
+          if (N_in == 0) begin
+            state <= DONE;
+            out <= 0;
+          end
+          else
+            state <= ITERATE;  
+        end
+      end
+      ITERATE : begin     
+        if (R > R_old || X_in[I_WIDTH+2*F_WIDTH-1 -: I_WIDTH+F_WIDTH] == X)
+          state <= DONE;
+        out <= out_in[I_WIDTH+2*F_WIDTH-1 -: I_WIDTH+F_WIDTH];
+      end
+      DONE : begin
+        state <= IDLE;
+      end
+      endcase
+    end
+  end
+    
+  always @ (*) begin
+    ready = 0;
+    out_valid = 0;
+    case (state) 
+     IDLE : ready = 1;
+     DONE : out_valid = 1;  
+    endcase 
+  end
+  
 endmodule
