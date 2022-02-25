73d4fbf62c396fb1db870f800df6c55d623d80fb
vn218 <72967949+vn218@users.noreply.github.com>
2022-02-25 21:22:41 +0530

    Update divider.v

--- design/divider.v
+++ design/divider.v
@@ -1,22 +1,29 @@
+
+// Uses Newton_Raphson Method
+// I_WIDTH, F_WIDTH ----> N_in and D_in (Q32.0 to Q4.28)
+// OUT_I_WIDTH, OUT_F_WIDTH ------> out (Q28.4 to Q4.28)
+//I_WIDTH + F_WIDTH = OUT_I_WIDTH + OUT_F_WIDTH = 32
+
+
 module divider
   #(parameter I_WIDTH = 16,
-   	      F_WIDTH = 16,
-              OUT_I_WIDTH = 16,
-              OUT_F_WIDTH = 16)  
- (
-  input [I_WIDTH+F_WIDTH-1:0] N_in, D_in,
+              F_WIDTH = 16,
+              OUT_I_WIDTH = 4,
+              OUT_F_WIDTH = 28)  
+(
+  input signed [I_WIDTH+F_WIDTH-1:0] N_in, D_in,
   input clk, rst, in_valid,
   output reg ready, out_valid,
-  output reg [I_WIDTH+F_WIDTH-1:0] out
+  output reg signed [I_WIDTH+F_WIDTH-1:0] out
 );
   
   parameter IDLE = 0,
-  	    SHIFT = 1,
+            SHIFT = 1,
             ITERATE = 2,
             DONE = 3;
   
-  localparam A = 32'b10_1101_0010_1101_0010,
-  	     B = 32'b01_1110_0001_1110_0001;
+  localparam A = 32'h2_D2D2D2D,
+             B = 32'h1_E1E1E1E;
   
   localparam iter = 4;
   
@@ -25,37 +32,56 @@ module divider
   
   
   reg signed [I_WIDTH+F_WIDTH-1:0] N, D, X;
-  wire signed [I_WIDTH+2*F_WIDTH-1:0] inter_1, feedback;
+  wire signed [OUT_I_WIDTH+2*OUT_F_WIDTH-1:0] inter_1, feedback;
   wire signed [I_WIDTH+F_WIDTH-1:0] X_in, inter_2;
-  wire signed [I_WIDTH+2*F_WIDTH-1:0] out_in;
+  reg signed [3*OUT_I_WIDTH+3*OUT_F_WIDTH-1:0] out_in , out_in_shift_1, out_in_shift_2;
   
   reg [1:0] state;
+  reg negative;
   
-  assign X_in = X + feedback[I_WIDTH+2*F_WIDTH-1 -: I_WIDTH+F_WIDTH];
+  assign X_in = X + feedback[OUT_I_WIDTH+2*OUT_F_WIDTH-1 -: I_WIDTH+F_WIDTH];
   assign inter_1 = D*X;
-  assign inter_2 = 32'b1_0000000000000000 -inter_1[I_WIDTH+2*F_WIDTH-1 -: I_WIDTH+F_WIDTH];
+  assign inter_2 = {{OUT_I_WIDTH-4{1'b0}},4'b1,{OUT_F_WIDTH{1'b0}}}  -inter_1[OUT_I_WIDTH+2*OUT_F_WIDTH-1 -: OUT_I_WIDTH+OUT_F_WIDTH];
   assign feedback = inter_2*X;
-  assign out_in = N*X_in;
+  
+  always @ (*) begin
+    
+    out_in = X_in*N;
+    
+    if (OUT_F_WIDTH > F_WIDTH) begin
+      out_in_shift_1 = out_in<<<(OUT_F_WIDTH-F_WIDTH);
+    end
+    else begin
+      out_in_shift_1 = out_in>>>(F_WIDTH-OUT_F_WIDTH);
+    end
+    
+    if (shift_counter <= I_WIDTH)
+      out_in_shift_2 <= out_in_shift_1 >>> (I_WIDTH - shift_counter);
+    else
+      out_in_shift_2 <= out_in_shift_1 <<< (shift_counter - I_WIDTH);
+  end
   
   always @ (posedge clk) begin
     case (state)
     IDLE : begin
       if (in_valid) begin
       	N <= N_in;
-        D <= D_in;
+        if (D_in < 0) begin
+          negative <= 1;
+           D <= -D_in;
+        end
+        else begin
+           negative <= 0;
+           D <= D_in;
+        end
         shift_counter <= 0;
         iter_counter <= 0;
       end
     end
     SHIFT : begin
       if ( D[I_WIDTH+F_WIDTH-1] == 1) begin
-        D <= {{I_WIDTH{1'b0}},D[I_WIDTH+F_WIDTH-1 -: F_WIDTH]};
-        X <=  A - B[I_WIDTH+F_WIDTH-1 -: I_WIDTH + (F_WIDTH/2)]*{{I_WIDTH{1'b0}},D[I_WIDTH+F_WIDTH-1 -: F_WIDTH/2]};
-        
-        if (shift_counter <= I_WIDTH)
-          N <= (N >> (I_WIDTH - shift_counter));
-        else
-          N <= (N << (shift_counter - I_WIDTH));
+        D <= {{OUT_I_WIDTH{1'b0}},D[I_WIDTH+F_WIDTH-1 -: OUT_F_WIDTH]};
+        X <=  {{OUT_I_WIDTH-4{1'b0}},A[I_WIDTH+F_WIDTH-1 -: 4+OUT_F_WIDTH]} - {{OUT_I_WIDTH-4{1'b0}},B[I_WIDTH+F_WIDTH-1 -: 4+(OUT_F_WIDTH)/2]}*{{OUT_I_WIDTH{1'b0}},D[I_WIDTH+F_WIDTH-1 -: OUT_F_WIDTH/2]};        
       end
       else begin
       	D <= D<<1;
@@ -63,9 +89,12 @@ module divider
       end
     end
     ITERATE : begin
-        X <= X_in;
-        out <= out_in[I_WIDTH+2*F_WIDTH-1 -: I_WIDTH+F_WIDTH];
-        iter_counter <= iter_counter + 1;        
+      X <= X_in;
+      if (negative)
+        out <= -out_in_shift_2[OUT_I_WIDTH+2*OUT_F_WIDTH-1 -: I_WIDTH+F_WIDTH];
+      else
+        out <= out_in_shift_2[OUT_I_WIDTH+2*OUT_F_WIDTH-1 -: I_WIDTH+F_WIDTH];
+      iter_counter <= iter_counter + 1;        
     end
     endcase
   
@@ -101,8 +130,8 @@ module divider
     ready = 0;
     out_valid = 0;
     case (state) 
-      IDLE : ready = 1;
-      DONE : out_valid = 1;  
+    IDLE : ready = 1;
+    DONE : out_valid = 1;  
 
     endcase
   end 
